e21c66cfa4f64afbc5a3fbe8b743750756509a10
Muhammad Abdul Monem <Eachawy@gmail.com>
2015-08-02 19:32:45 +0200

    controller with directives

--- Booking_App/app/Scripts/controllers/controller.js
+++ Booking_App/app/Scripts/controllers/controller.js
@@ -2,11 +2,23 @@
 
 var HotelsCtrls = angular.module('BookingApp.Ctrl', []);
 
+	//pagination ------------------
+	    HotelsCtrls.filter('offset', function () {
+	        return function (input, start) {
+	            start = parseInt(start, 10);
+	            return input.slice(start);
+	        };
+	    });
+	//pagination ------------------
 
-HotelsCtrls.controller('AllHotels', ['$scope','Hotels', 
-	function ($scope,Hotels) {
-		//alert(Hotels().length);
-		$scope.Hotels = Hotels;
-		//console.log($scope.Hotels());
-}])
+HotelsCtrls.controller('AllHotels', ['$scope', 'Hotels',
+	function ($scope, Hotels) {
+	    $scope.Hotels = [];
+	    $scope.itemsPerPage = 6;
+	    $scope.currentPage = 0;
+	        Hotels.query(function (data) {
+	            $scope.Hotels = data;
+	            $scope.allItemsCount = $scope.Hotels.length;
+	        });
+	}]);
 
