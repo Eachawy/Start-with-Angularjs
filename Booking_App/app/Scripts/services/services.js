de8e6cde12455f0edda32311bf9d534de63bb5d5
Muhammad Abdul Monem <Eachawy@gmail.com>
2015-08-02 19:31:41 +0200

    new modification

--- Booking_App/app/Scripts/services/services.js
+++ Booking_App/app/Scripts/services/services.js
@@ -4,16 +4,7 @@ var HServices = angular.module('BookingApp.Services', ['ngResource']);
 
 HServices.factory('Hotels', ['$resource',
 	function ($resource) {
-		var Hotels =  $resource('app/Data/Reviews.json', {}, {
-			      query: {method:'GET'},
-			      queryAll: {method:'GET', isArray: true},
-			      create: {method:'POST'},
-			      update: {method: 'PUT'}
-			  });
-		return Hotels;
+	    return  $resource('app/Data/Reviews.json');
 }]);
-
-
-
 
 
