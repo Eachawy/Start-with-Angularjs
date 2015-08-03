243ab0a95074dcde0c85c496304b08553cf0e6fa
Muhammad Abdul Monem <Eachawy@gmail.com>
2015-08-03 18:17:11 +0200

    Update app.js

--- Booking_App/app/app.js
+++ Booking_App/app/app.js
@@ -1,16 +1,37 @@
 'use strict';
+'use strict';
 
-angular.module('BookingApp', ['ngRoute','BookingApp.Services','BookingApp.Ctrl', 'Hotels.dir'])
+angular.module('BookingApp', ['ngRoute', 'BookingApp.Services', 'BookingApp.Ctrl', 'Hotels.dir'])
 	.config(function ($routeProvider) {
 		$routeProvider.
-			when('/', {
-				templateUrl: 'app/views/main.html',
-				controller: 'AllHotels'
+			when('/main', {
+			templateUrl: 'app/views/main.html',
+			controller: 'AllHotels'
+			}).
+			when('/Details', {
+			    templateUrl: 'app/views/HotelsDetails.html',
+			    controller: ''
+			}).
+			when('/Add', {
+			    templateUrl: 'app/views/AddEdit.html',
+			    controller: ''
+			}).
+			when('/Edit', {
+			    templateUrl: 'app/views/AddEdit.html',
+			    controller: ''
+			}).
+			when('/view', {
+			    templateUrl: 'app/views/viewlist.html',
+			    controller: ''
 			}).
 			otherwise({ 
-				redirectTo: '/' 
+			    redirectTo: '/main'
 			})
 	});
+
+
+
+
 
 
 
