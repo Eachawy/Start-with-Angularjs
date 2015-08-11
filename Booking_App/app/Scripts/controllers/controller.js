221ad5ea3882b1ae5833cd1d953c12118d391ad4
Muhammad Abdul Monem <Eachawy@gmail.com>
2015-08-11 17:26:18 +0200

    Update controller.js

--- Booking_App/app/Scripts/controllers/controller.js
+++ Booking_App/app/Scripts/controllers/controller.js
@@ -17,8 +17,20 @@ HotelsCtrls.controller('AllHotels', ['$scope', 'Hotels',
 	    $scope.itemsPerPage = 6;
 	    $scope.currentPage = 0;
 	        Hotels.query(function (data) {
+
+            // Filter by javascript ---------------------------------------
+	            //var invalidEntries = 0;function filterByID(obj) { if ((obj.ID) == 'H50001') { return true; } else { invalidEntries++; return false; } };
+	            //$scope.Hotels = data.filter(filterByID)
+	        // Filter by javascript ---------------------------------------
+
 	            $scope.Hotels = data;
 	            $scope.allItemsCount = $scope.Hotels.length;
-	        });
+	    });
+
+	    // Filter by angularjs -------------------------------
+	        //$scope.customArrayFilter = function (item) {
+	        //    return (item.ID == 'H50001');
+	        //};
+	    // Filter by angularjs -------------------------------
 	}]);
 
